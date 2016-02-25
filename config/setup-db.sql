925f156bf66ec4c08f7eac5eaa85273ba36b8714
Andrew Khmylov <andrew.khmylov@gmail.com>
2016-02-25 20:02:05 +0300

    Add another test client

--- config/setup-db.sql
+++ config/setup-db.sql
@@ -19,6 +19,10 @@ INSERT INTO
     clients (name, client_key, client_secret, redirect_uri, description)
     VALUES ('Test Application #2', 'testApp2', 'testApp2Secret', 'http://localhost:3002/callback', 'This is the second test application');
 
+INSERT INTO
+    clients (name, client_key, client_secret, redirect_uri, description)
+    VALUES ('Targetprocess Views experiments', 'temp-tpViewsExperiments', 'temp-tpViewsExperimentsSecret', 'http://tp-experiments.herokuapp.com/auth/callback', 'Experimental things with Targetprocess views');
+
 CREATE TABLE access_tokens (
     id SERIAL PRIMARY KEY,
     token TEXT UNIQUE NOT NULL,
